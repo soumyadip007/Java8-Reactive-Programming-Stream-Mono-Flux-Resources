be33d20d91980a875cf721e1dc37d936bb6df6ca
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-05-31 23:49:48 +0530

    feat : user

---
+++ practive/src/main/java/reactive/practive/User.java
@@ -0,0 +1,49 @@
+package reactive.practive;
+
+
+public class User {
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 22222!" );
+    }
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
+    }
+}
